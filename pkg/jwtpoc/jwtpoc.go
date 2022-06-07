8300a469d111d0ec559bde0c911f2f559f0786f7
Robert Sigler <sigler@improvisedscience.org>
2022-06-07 17:35:49 -0400

    Adding remarks and updating depricated code

--- pkg/jwtpoc/jwtpoc.go
+++ pkg/jwtpoc/jwtpoc.go
@@ -8,18 +8,22 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// Options for scrypt
+// Options for jwtpoc
 type Option func(c *Config)
+
+// Config for jwtpoc
 type Config struct {
 	log    *logrus.Logger
 	secret []byte
 }
 
+// CustomClaims is a struct that contains the custom claims for the JWT
 type CustomClaims struct {
 	UserId string `json:"userid"`
-	jwt.StandardClaims
+	jwt.RegisteredClaims
 }
 
+// New creates a new jwtpoc config
 func New(opts ...func(*Config)) (*Config, error) {
 	config := &Config{}
 
@@ -34,28 +38,31 @@ func New(opts ...func(*Config)) (*Config, error) {
 	return config, nil
 }
 
+// SetLogger sets the logger for the jwtpoc
 func SetLog(log *logrus.Logger) Option {
 	return func(c *Config) {
 		c.log = log
 	}
 }
 
+// SetSecret sets the secret for the jwtpoc
 func SetSecret(secret string) Option {
 	return func(c *Config) {
 		c.secret = []byte(secret)
 	}
 }
 
+// CreateToken creates a new JWT token
 func (c *Config) Create(userid string) (string, error) {
 	// Create a new token object, specifying signing method and the claims
 	// you would like it to contain.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &CustomClaims{
 		UserId: userid,
-		StandardClaims: jwt.StandardClaims{
+		RegisteredClaims: jwt.RegisteredClaims{
 			// Set the token expiration to 1 day
-			ExpiresAt: jwt.TimeFunc().Add(24 * time.Hour).Unix(),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
 			Issuer:    "FindYourVote",
-			IssuedAt:  jwt.TimeFunc().Unix(),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
 		},
 	})
 
@@ -63,11 +70,12 @@ func (c *Config) Create(userid string) (string, error) {
 	return token.SignedString(c.secret)
 }
 
+// ValidateToken validates a JWT token
 func (c *Config) Validate(tokenString string) (*CustomClaims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return c.secret, nil
 	})
